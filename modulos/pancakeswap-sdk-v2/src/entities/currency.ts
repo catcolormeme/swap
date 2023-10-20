dda17930d8b1ecf8d6492dcb5c046c05fa81521d
powman <paulophpweb@gmail.com>
2023-10-19 22:37:27 -0300

    Private

---
+++ modulos/pancakeswap-sdk-v2/src/entities/currency.ts
@@ -0,0 +1,38 @@
+import env from '../../env.json'
+import JSBI from 'jsbi'
+
+import { SolidityType } from '../constants'
+import { validateSolidityTypeInstance } from '../utils'
+
+/**
+ * A currency is any fungible financial instrument on Ethereum, including Ether and all ERC20 tokens.
+ *
+ * The only instance of the base class `Currency` is Ether.
+ */
+export class Currency {
+  public readonly decimals: number
+  public readonly symbol?: string
+  public readonly name?: string
+
+  /**
+   * The only instance of the base class `Currency`.
+   */
+  public static readonly ETHER: Currency = new Currency(env.REACT_APP_NATIVE_TOKEN_DECIMAL as unknown as number, env.REACT_APP_NATIVE_TOKEN_SYMBOL as string, env.REACT_APP_NATIVE_TOKEN_NAME as string)
+
+  /**
+   * Constructs an instance of the base class `Currency`. The only instance of the base class `Currency` is `Currency.ETHER`.
+   * @param decimals decimals of the currency
+   * @param symbol symbol of the currency
+   * @param name of the currency
+   */
+  protected constructor(decimals: number, symbol?: string, name?: string) {
+    validateSolidityTypeInstance(JSBI.BigInt(decimals), SolidityType.uint8)
+
+    this.decimals = decimals
+    this.symbol = symbol
+    this.name = name
+  }
+}
+
+const ETHER = Currency.ETHER
+export { ETHER }
---
+++ src/constants/multicall/index.ts
@@ -0,0 +1,8 @@
+import { ChainId } from '@pancakeswap-libs/sdk-v2'
+import MULTICALL_ABI from './abi.json'
+
+const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
+  [ChainId.PRIVATE]: process.env.REACT_APP_MULTICALL_ADDRESS as string,
+}
+
+export { MULTICALL_ABI, MULTICALL_NETWORKS }
